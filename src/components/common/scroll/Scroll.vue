7db454eb6f889153d85765ddd9a253fc5ae63175
penny2019 <2579817689@qq.com>
2020-02-12 18:02:48 +0800

    '代码提交信息'

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll";
+export default {
+  name: "Scroll",
+  props: {
+    probeType: {
+      type: Number,
+      default: 0
+    },
+    pullUpLoad: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      scroll: null
+    };
+  },
+  methods: {
+    scrollTo(x, y, time = 300) {
+      this.scroll.scrollTo(x, y, time);
+    },
+    finishPullUp() {
+      this.scroll.finishPullUp();
+    }
+  },
+  mounted() {
+    // 1.创建BScroll对象
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      probeType: this.probeType,
+      pullUpLoad: this.pullUpLoad,
+      click: true
+    });
+    // 2.监听滚动的位置
+    this.scroll.on("scroll", position => {
+      this.$emit("scroll", position);
+    });
+    // 3.监听上拉事件
+    this.scroll.on("pullingUp", () => {
+      this.$emit("PullingUp");
+    });
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/content/goods/GoodsListItem.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="goods_item">
+    <img :src="goodsItem.show.img" alt />
+    <div class="goods_info">
+      <p>{{goodsItem.title}}</p>
+      <span class="price">{{goodsItem.price}}</span>
+      <span class="collect">{{goodsItem.cfav}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoodsListItem",
+  props: {
+    goodsItem: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.goods_item {
+  padding-bottom: 40px;
+  position: relative;
+  width: 49%;
+}
+.goods_item img {
+  width: 100%;
+  border-radius: 5px;
+}
+.goods_info {
+  font-size: 12px;
+  position: absolute;
+  bottom: 5px;
+  left: 0;
+  right: 0;
+  text-align: center;
+  overflow: hidden;
+}
+.goods_info p {
+  /* 文字溢出使用省略号显示 */
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin-bottom: 3px;
+}
+.goods_info .price {
+  color: var(--color-high-text);
+  margin-right: 20px;
+}
+.goods_info .collect {
+  position: relative;
+}
+.goods_info .collect::before {
+  content: "";
+  position: absolute;
+  top: -1px;
+  left: -15px;
+  width: 14px;
+  height: 14px;
+  background: url("~assets/img/common/collect.svg") 0 0/14px 14px;
+}
+</style>
